a69c2cb5f8a5f95c32936d12ae72d369dbb03b8d
Sai-Ly <kcijogm@gmail.com>
2025-10-17 23:49:59 +0800

    Completed Exercise 2: Simple Sums

---
+++ 02-SimpleSums.py
@@ -0,0 +1,11 @@
+#Exercise 2: The sum of 8 and 10
+# Name: Kcijo Galapate
+
+a = 8   #added a value to a variable
+b = 10
+c = a+b   #prints the total sum of the two positive integers
+
+print("THE SUM OF TWO NUMBERS")
+print("The first digit:", a)  #prints the value of variable 'a'
+print("The second digit:", b) #prints the value of variable 'b'
+print("The total sum is =", c ) #prints the total of the value 'a' and 'b'
