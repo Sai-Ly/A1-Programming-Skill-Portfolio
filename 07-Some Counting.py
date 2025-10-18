cf2a7d022c203c8174cd0d11b1bafd1eaa2c044c
Sai-Ly <kcijogm@gmail.com>
2025-10-19 00:17:22 +0800

    Completed Exercise 7: Some Counting

---
+++ 07-Some Counting.py
@@ -0,0 +1,31 @@
+count1 = 0
+print("-----Counting 0 up to 50-----")
+while count1 <= 50:
+    print (count1)
+    count1 += 1 
+
+print("\n-----Counting 50 down to 0-----")
+count2 = 50
+while count2 >= 0:
+    print (count2)
+    count2 -= 1
+
+print("\n-----Counting 30 up to 50-----")
+count3 = 30
+while count3 <= 50:
+    print(count3)
+    count3 += 1
+    
+print("\n-----Counting down from 50 to 10 w/ decrements of 2-----")
+count4 = 50
+while count4 >= 10:
+    print (count4)
+    count4 -= 2
+    
+print("\n-----Counting up from 100 to 200 in increments of 5-----")
+count5 = 100
+while count5 <= 200:
+    print(count5)
+    count5 += 5
+    
+print("-Counting Done!-")
