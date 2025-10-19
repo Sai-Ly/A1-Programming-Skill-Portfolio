b2aa96476e3e3217b3dac003570943067f6dd3b1
Sai-Ly <kcijogm@gmail.com>
2025-10-20 01:25:41 +0800

    Completed Exercise 10: Is It Even?

---
+++ 10-Is It Even?.py
@@ -0,0 +1,14 @@
+#Exercise:10
+#Is It Even?
+
+
+def main():
+    a = float(input("Enter First number:"))
+    b = float(input("Enter second number:"))
+
+    if a == b:
+        print("Values are even!")
+    else:
+        print("Values are odd!")
+if __name__ == "__main__":
+    main()
