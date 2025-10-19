929486fc7ce39bc26ae474cedf25c33bb6e7f516
Sai-Ly <kcijogm@gmail.com>
2025-10-20 00:47:05 +0800

    Completed Exercise 9: Hello

---
+++ 09-Hello.py
@@ -0,0 +1,11 @@
+#Exercise 9: Hello
+#Kcijo Galapate
+
+
+def hello():
+    print("Hello! How are you today!")
+def main():
+    hello()
+    print("Hope you're doing well c:")
+if __name__ == "__main__":
+    main()
