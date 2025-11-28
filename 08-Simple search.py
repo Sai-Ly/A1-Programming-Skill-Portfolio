db1a98f92f4c6767e040215aef50e5e20dd25628
Sai-Ly <kcijogm@gmail.com>
2025-11-29 03:36:08 +0800

    Completed Exercise 8: Simple Search

--- 08-Simple search.py
+++ 08-Simple search.py
@@ -1,7 +1,10 @@
-characters = ["Jake" "Zac", "Ian", "Ron", "Sam", "Dave"]
-search = input("Who should you be looking for?:")
+#Kcijo Galapate
+
+
+characters = ["Jake" "Zac", "Ian", "Ron", "Sam", "Dave"] #making a list of names with the variable named "Characters"
+search = input("Who should you be looking for?:") #user input
 print("-------------------------------")
-if search in characters and search == "Sam":
-     print("Found Sam on the list!")
-else:
-    print("You searched the wrong name.")
+if search in characters and search == "Sam": #stating that if the search (a user input) in characters ( list of names) matches Sam
+     print("Found Sam on the list!") #Found Sam
+else: #saying otherwise
+    print("You searched the wrong name.") #Wrong name was entered
