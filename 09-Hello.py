0898cbe7db7f506d4dd0384c4c2bf118be1a7576
Sai-Ly <kcijogm@gmail.com>
2025-11-29 03:39:47 +0800

    Completed Exercise 9: Hello

--- 09-Hello.py
+++ 09-Hello.py
@@ -2,10 +2,10 @@
 #Kcijo Galapate
 
 
-def hello():
-    print("Hello! How are you today!")
-def main():
-    hello()
+def hello(): #defining a function called hello
+    print("Hello! How are you today #prints output
+def main(): #Used for starting points
+    hello() #only runs when hello is called
     print("Hope you're doing well c:")
-if __name__ == "__main__":
+if __name__ == "__main__": #check if its beiing run directly in main
     main()
