a116ecebe238637f3d8020b178d2ef755a642d70
Sai-Ly <kcijogm@gmail.com>
2025-11-29 03:32:49 +0800

    Update 07-Some Counting.py

--- 07-Some Counting.py
+++ 07-Some Counting.py
@@ -1,31 +1,36 @@
-count1 = 0
+#Kcijo Galapate
+
+
+
+
+count1 = 0 #base of counting 0 up to 50
 print("-----Counting 0 up to 50-----")
-while count1 <= 50:
-    print (count1)
-    count1 += 1 
+while count1 <= 50: #if the variable is less than or equal to 50
+    print (count1) #start of counting
+    count1 += 1 #incrementing each number up to 1
 
 print("\n-----Counting 50 down to 0-----")
-count2 = 50
-while count2 >= 0:
-    print (count2)
-    count2 -= 1
+count2 = 50  #base of counting 50 down to 0
+while count2 >= 0: #if the variable is greater than or equal to 0
+    print (count2) #start of countdown
+    count2 -= 1  #decrementing each number down to 1
 
 print("\n-----Counting 30 up to 50-----")
-count3 = 30
-while count3 <= 50:
-    print(count3)
-    count3 += 1
+count3 = 30 #base of counting 30 up to 50
+while count3 <= 50: #if the variable is less than or equal to 50
+    print(count3)  #start of counting
+    count3 += 1 #decrementing each number down to 1
     
 print("\n-----Counting down from 50 to 10 w/ decrements of 2-----")
-count4 = 50
-while count4 >= 10:
-    print (count4)
-    count4 -= 2
+count4 = 50 #base of counting down from 50 to 10
+while count4 >= 10: #if the variable is greater than or equal to 10
+    print (count4) #start of countdown
+    count4 -= 2 #decrements of 2
     
 print("\n-----Counting up from 100 to 200 in increments of 5-----")
-count5 = 100
-while count5 <= 200:
-    print(count5)
-    count5 += 5
+count5 = 100 #base of counting up from 100 to 200
+while count5 <= 200: #if the variable is less than or equal to 200
+    print(count5) #start of coutning
+    count5 += 5 #increments of 5
     
 print("-Counting Done!-")
