8ab7a7e091a1fbf5b0835da8598bb97179f032b5
Sai-Ly <kcijogm@gmail.com>
2025-11-29 03:42:53 +0800

    Completed Exercise 10: Is It Even?

--- 10-Is It Even?.py
+++ 10-Is It Even?.py
@@ -2,13 +2,13 @@
 #Is It Even?
 
 
-def main():
-    a = float(input("Enter First number:"))
-    b = float(input("Enter second number:"))
+def main(): #defining a main function
+    a = float(input("Enter First number:")) #user enters a number
+    b = float(input("Enter second number:")) #user enters a second number
 
-    if a == b:
-        print("Values are even!")
-    else:
-        print("Values are odd!")
-if __name__ == "__main__":
+    if a == b: # if a is the same as a and b
+        print("Values are even!") #prints value are even
+    else: #otherwise
+        print("Values are odd!") #others are odd
+if __name__ == "__main__": #checks if program is run in the main
     main()
