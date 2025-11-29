56f56238eceeb46d68012aae995a6d32231294c1
Sai-Ly <kcijogm@gmail.com>
2025-11-29 19:58:12 +0800

    Completed Exercise 1: CodingIsCool

--- 01-CodingIsCool.py
+++ 01-CodingIsCool.py
@@ -1,9 +1,10 @@
 #Exercise 1: Coding is Cool
 # Kcijo Galapate
 
-print("----OTPUT THE PHASE----") #print() what to show on the screen
-word1 = "Coding" #add variable which is 'word1' and a value which is 'Coding'
+print("----OTPUT THE PHASE----") #print() what to display on the screen
+#define variables and assign values
+word1 = "Coding" 
 word2 = "Is"
 word3 = "Cool"
 
-print(word1, word2, word3) #prints the outcome of variables and values together
+print(word1, word2, word3) #prints the outcome of variables and values together as phrase
